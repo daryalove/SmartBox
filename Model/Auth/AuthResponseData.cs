fbfc9448023cf1248d2f4b0d92dbd77f7ff21646
Darya Yakovleva <50288548+daryalove@users.noreply.github.com>
2019-10-28 20:52:45 +0300

    Add files via upload

---
+++ Model/Auth/AuthResponseData.cs
@@ -0,0 +1,39 @@
+﻿namespace GeoGeometry.Model.Auth {
+	public class AuthResponseData: BaseResponseObject
+    {
+        /// <summary>
+        /// Код клиента.
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Email клиента.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Имя клиента.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Фамилия клиента.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Долгота
+        /// </summary>
+        public double Longitude { get; set; }
+
+        /// <summary>
+        /// Широта
+        /// </summary>
+        public double Latitude { get; set; }
+
+        /// <summary>
+        /// Дистанция
+        /// </summary>
+        public double Distance { get; set; }
+    }
+}
